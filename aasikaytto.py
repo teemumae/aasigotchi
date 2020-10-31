ca1a7b568354867bd21dbb3ded983bb83f0bc847
Teemu Mäenpää <teemumaen@gmail.com>
2020-10-31 13:18:30 +0200

    Kommentit aasikayttoon ja mainin aloitus

--- aasikaytto.py
+++ aasikaytto.py
@@ -1,10 +1,9 @@
 def nayta_tila(tila):
-"""
-Tulostaa aasin tilan.
-"""
-
+    """
+    Tulostaa aasin tilan.
+    """
 def pyyda_syote(tila):
-"""
-Näyttää käyttäjälle aasin tilaa vastaavat mahdolliset syötteet ja kysyy uutta
-syötettä kunnes käyttäjä antaa laillisen syötteen. Saatu syöte palautetaan.
-"""       
+    """
+    Näyttää käyttäjälle aasin tilaa vastaavat mahdolliset syötteet ja kysyy uutta
+    syötettä kunnes käyttäjä antaa laillisen syötteen. Saatu syöte palautetaan.
+    """       
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import aasilogiikka
 import aasimaaritelmat
 
 def main():
-    aasidata = aasilogiikka.???
+    aasidata = aasilogiikka.alusta
     
     while True:
         aasikaytto.nayta_tila(aasidata)
@@ -12,13 +12,17 @@ def main():
         if syote == aasimaaritelmat.LOPETA:
             break
         elif syote == aasimaaritelmat.RUOKI:
-            ???
+            aasilogiikka.ruoki
+            #???
         elif syote == aasimaaritelmat.KUTITA:
-            ???
+            #???
+            break
         elif syote == aasimaaritelmat.TYOSKENTELE:
-            ???
+            #???
+            break
         elif syote == aasimaaritelmat.ALUSTA:
-            ???
+            #???
+            break
 
 if __name__ == "__main__":
-    ???
+   # ???
