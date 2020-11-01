c846ce28f95657cf0475ac51174cf67ae8d4e33e
Teemu Mäenpää <teemumaen@gmail.com>
2020-11-01 13:09:51 +0200

    Tila ja syöte koodia

--- aasikaytto.py
+++ aasikaytto.py
@@ -1,11 +1,19 @@
 import aasilogiikka
 import aasimaaritelmat
-def nayta_tila(tila):
-    """
-    Tulostaa aasin tilan.
-    """
+
 def pyyda_syote(tila):
     """
     Näyttää käyttäjälle aasin tilaa vastaavat mahdolliset syötteet ja kysyy uutta
     syötettä kunnes käyttäjä antaa laillisen syötteen. Saatu syöte palautetaan.
-    """       
+    """
+    print("Valinnat: q, r, k, t")
+    syote = input("Anna seuraava valinta: ")
+    return syote
+def nayta_tila(tila):
+    """
+    Tulostaa aasin tilan.
+    """
+    print("Aasi on {} vuotta vanha ja rahaa on {} mk.".format(tila['IKÄ'], tila['RAHA']))
+    print("Kylläisyys: {}".format(tila['KYLLÄISYYS']))
+    print("Onnellisuus:{}".format(tila['ONNELLISUUS']))
+    print("Jaksaminen:{}".format(tila['JAKSAMINEN']))
